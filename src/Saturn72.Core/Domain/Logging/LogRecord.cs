8dccd8628cef69ef5f86298f81cfb4ffabd18452
rois <roi@saturn72.com>
2015-11-24 18:45:37 +0200

    move git ignore files

---
+++ src/Saturn72.Core/Domain/Logging/LogRecord.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Saturn72.Core.Domain.Users;
+
+namespace Saturn72.Core.Domain.Logging
+{
+    public class LogRecord : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the log level identifier
+        /// </summary>
+        public int LogLevelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the short message
+        /// </summary>
+        public string ShortMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full exception
+        /// </summary>
+        public string FullMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log level
+        /// </summary>
+        public LogLevel LogLevel
+        {
+            get
+            {
+                return (LogLevel)LogLevelId;
+            }
+            set
+            {
+                LogLevelId = (int)value;
+            }
+        }
+        public IDictionary<object, object> ExtraData { get; set; }
+    }
+}
---
+++ wiki/Home.md
@@ -0,0 +1,6 @@
+Welcome to the Saturn72.Core wiki!
+
+Saturn72.Core are the core libraries of Saturn72.Web. 
+The next sections explains how to use, extend and replace the exist functionality easily and quickly
+
+[Understanding Logger]

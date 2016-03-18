d79754a6bd36c1bc10bc29536ef43a471e6d0e17
roi shabtai <roi@saturn72.com>
2016-03-19 00:53:51 +0200

    Create default startup application

---
+++ src/Common/Saturn72.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Saturn72.App.Common;
+
+namespace Saturn72.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var args = Environment.GetCommandLineArgs();
+            var appId = args.Length >= 2 ? args[1] : AppDomain.CurrentDomain.FriendlyName;
+
+            var app = new Saturn72App(appId);
+            app.Start();
+        }
+    }
+}
